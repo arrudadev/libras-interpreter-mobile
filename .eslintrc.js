963f5979119ba204bcf94989f974ab48dd0aa2eb
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2024-01-15 16:22:43 -0300

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    '@rocketseat/eslint-config/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react'],
+  rules: {},
+}
--- App.tsx
+++ App.tsx
@@ -1,30 +1,36 @@
-import { useEffect, useRef, useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { Camera, CameraType as ExpoCameraType } from 'expo-camera';
-import { Ionicons } from '@expo/vector-icons';
+import { useEffect, useRef, useState } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { Camera, CameraType as ExpoCameraType } from 'expo-camera'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(false);
-  const [cameraType, setCameraType] = useState(ExpoCameraType.back);
-  const cameraRef = useRef<Camera>(null);
+  const [hasPermission, setHasPermission] = useState(false)
+  const [cameraType, setCameraType] = useState(ExpoCameraType.back)
+  const cameraRef = useRef<Camera>(null)
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View />;
-  }
-
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+  async function requestCameraPermission() {
+    const { status } = await Camera.requestCameraPermissionsAsync()
+    setHasPermission(status === 'granted')
   }
 
   function flipCamera() {
-    setCameraType(cameraType === ExpoCameraType.back ? ExpoCameraType.front : ExpoCameraType.back)
+    setCameraType(
+      cameraType === ExpoCameraType.back
+        ? ExpoCameraType.front
+        : ExpoCameraType.back,
+    )
+  }
+
+  useEffect(() => {
+    requestCameraPermission()
+  }, [])
+
+  if (hasPermission === null) {
+    return <View />
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>
   }
 
   return (
@@ -36,18 +42,20 @@ export default function App() {
             justifyContent: 'flex-end',
             paddingTop: 45,
             paddingRight: 15,
-          }}>
+          }}
+        >
           <TouchableOpacity
             style={{
               backgroundColor: 'gray',
               borderRadius: 50,
               padding: 10,
             }}
-            onPress={flipCamera}>
+            onPress={flipCamera}
+          >
             <Ionicons name="camera-reverse-outline" size={24} color="white" />
           </TouchableOpacity>
         </View>
       </Camera>
     </View>
-  );
+  )
 }
